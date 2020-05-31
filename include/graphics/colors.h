c2408feb051afd1b1c4410c79f3e6a29c5b48fdb
Pradeep <pradeep050997@gmail.com>
2020-05-31 02:38:42 -0400

    Add an assortment of common colours

---
+++ include/graphics/colors.h
@@ -0,0 +1,14 @@
+// Copyright 2020 | Pradeep Suresh | University of Michigan
+#pragma once
+#include "../structures/bmp.h"
+
+color_bgr_t white = {255, 255, 255};
+color_bgr_t black = {0, 0, 0};
+
+color_bgr_t blue = {255, 0, 0};
+color_bgr_t green = {0, 255, 0};
+color_bgr_t red = {0, 0, 255};
+
+color_bgr_t yellow = {0, 255, 255};
+color_bgr_t sky_blue = {255, 255, 0};
+color_bgr_t pink = {255, 0, 255};
