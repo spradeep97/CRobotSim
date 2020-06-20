570d5c3c09bad1f35908867de94cd289b0f4470f
Pradeep Suresh <spradeep@umich.edu>
2020-06-19 20:41:44 -0400

    Keep only slow mode

--- apps/braitenberg/braitenberg.c
+++ apps/braitenberg/braitenberg.c
@@ -16,17 +16,10 @@ color_bgr_t pink = {255, 0, 255};
 
 int main(int argc, char *argv[]) {
     int args_read = 0;
-    int fast_mode = 0;
     int time_steps = 0;
 
     // Read command line arguments
     args_read = sscanf(argv[1], "%d", &time_steps);
-    if (args_read <= 0) {
-        fprintf(stderr, "usage: ./braitenberg <time steps> <fast=0|1>\n");
-        return 1;
-    }
-
-    args_read = sscanf(argv[2], "%d", &fast_mode);
     if (args_read <= 0) {
         fprintf(stderr, "usage: ./braitenberg <time steps> <fast=0|1>\n");
         return 1;
@@ -40,37 +33,34 @@ int main(int argc, char *argv[]) {
     gx_create_bmp(&bmp, 640, 480);
     gx_clear(&bmp, black);
 
-    if (fast_mode == 0) {
-        // Create time interval structure
-        int64_t seconds = 0;
-        int64_t nanoseconds = 40 * 1000 * 1000;
-        struct timespec interval = {seconds, nanoseconds};
+    // Create time interval structure
+    int64_t seconds = 0;
+    int64_t nanoseconds = 40 * 1000 * 1000;
+    struct timespec interval = {seconds, nanoseconds};
 
-        // Initialize world and graphics
-        init_world(&world);
-        init_graphics(&world, &bmp);
+    // Initialize world and graphics
+    init_world(&world);
+    init_graphics(&world, &bmp);
 
-        // Create space in memory for bitmap's contents
-        size_t bmp_size = bmp_calculate_size(&bmp);
-        uint8_t *serialized_bmp = malloc(bmp_size);
+    // Create space in memory for bitmap's contents
+    size_t bmp_size = bmp_calculate_size(&bmp);
+    uint8_t *serialized_bmp = malloc(bmp_size);
 
-        // Main loop
-        for (int t_step = 0; t_step < time_steps; t_step++) {
-            moveTo_nextRobotPose(&world);
-            resolve_collision(&world);
-            update_graphics(&world, &bmp);
-            bmp_serialize(&bmp, serialized_bmp);
-            image_server_set_data(bmp_size, serialized_bmp);
-            image_server_start("8000");
-            nanosleep(&interval, NULL);
-        }
-
-        destroy_world(&world);
-        free(bmp.data);
-        free(serialized_bmp);
-
-    } else {
-
+    // Main loop
+    for (int t_step = 0; t_step < time_steps; t_step++) {
+        moveTo_nextRobotPose(&world);
+        resolve_collision(&world);
+        update_graphics(&world, &bmp);
+        bmp_serialize(&bmp, serialized_bmp);
+        image_server_set_data(bmp_size, serialized_bmp);
+        image_server_start("8000");
+        nanosleep(&interval, NULL);
     }
+
+    // Free all malloced memory
+    destroy_world(&world);
+    free(bmp.data);
+    free(serialized_bmp);
+
     return 0;
 }
